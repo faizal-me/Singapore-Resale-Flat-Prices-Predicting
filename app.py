9324aa27d0b24da46f74e5e5b3e431b090fc8d4e
Faizal S <107980267+faizal-me@users.noreply.github.com>
2025-01-25 17:40:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ if 'input_values' not in st.session_state:
     }
 
 if section == "🏠 Home":
-    st.image("Singapore.webp", caption="Singapore", use_column_width=True)  
+    st.image("Singapore.webp", caption="Singapore", use_container_width=True)  
     st.markdown("### About This Application")
     st.write(
         """
@@ -150,8 +150,4 @@ elif section == "💵 Predict Resale Price":
             predicted_price = pipeline.predict(input_data)[0]
             st.success(f"🎉 Predicted Resale Price: ${predicted_price:.2f}")
 
-        # Download button for result
-        download_df = input_data.copy()
-        download_df['predicted_resale_price'] = predicted_price
-        csv = download_df.to_csv(index=False)
-        st.download_button(label="Download Prediction Results", data=csv, file_name='resale_price_prediction.csv', mime='text/csv')
+    
